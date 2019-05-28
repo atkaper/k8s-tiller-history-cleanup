51ac4b3abce4670f484db77ba9c06ce854e65d3b
Thijs Kaper <atkaper@gmail.com>
2019-05-28 13:49:58 +0200

    Initial version

--- README.md
+++ README.md
@@ -1,2 +1,56 @@
 # k8s-tiller-history-cleanup
-Kubernetes Tiller/Helm Release History Cleanup
+
+Kubernetes Tiller/Helm Release History Cleanup, a script to cleanup old helm tiller history config maps.
+
+This can be handy if you forgot to set TILLER_HISTORY_MAX on your tiller deployment, and added it later on.
+Or... if you remove components using kubectl delete instead of using helm delete (we do remove some namespaces
+automatically, which leaves unused tiller configmaps in the system).
+
+In the script, change the two variables below to match your setup.
+```
+TILLERNAMESPACE=kube-system
+TILLER_HISTORY_MAX=5
+```
+
+In our cluster, I used the following command to alter the history size:
+```
+kubectl edit deploy -n kube-system tiller-deploy
+```
+
+This is the env fragment:
+```
+      - env:
+        - name: TILLER_NAMESPACE
+          value: kube-system
+        - name: TILLER_HISTORY_MAX
+          value: "5"
+```
+
+The script can be called using: ./tiller-cleanup.sh with a single parameter of -f or -n:
+```
+Usage: ./tiller-cleanup.sh [-f | -n]
+Use -n for a dry-run to show what would be deleted
+Use -f to execute a run with actual deletes
+```
+
+Note: the script also queries the system for ALL components which are tagged with heritage=Tiller,
+to find if there is an active helm deploy for a certain release. If not found, the helm history
+will be removed.
+
+Please be carefull, and first execute a dry-run to see if the script will remove the right
+data for your setup.
+
+For reference, the versions of helm/k8s on which we use this:
+```
+$ helm version
+Client: &version.Version{SemVer:"v2.8.2", GitCommit:"a80231648a1473929271764b920a8e346f6de844", GitTreeState:"clean"}
+Server: &version.Version{SemVer:"v2.8.2", GitCommit:"a80231648a1473929271764b920a8e346f6de844", GitTreeState:"clean"}
+
+$ kubectl version
+Client Version: version.Info{Major:"1", Minor:"8", GitVersion:"v1.8.5", GitCommit:"cce11c6a185279d037023e02ac5249e14daa22bf", GitTreeState:"clean", BuildDate:"2017-12-07T16:16:03Z", GoVersion:"go1.8.3", Compiler:"gc", Platform:"linux/amd64"}
+Server Version: version.Info{Major:"1", Minor:"8", GitVersion:"v1.8.1+coreos.0", GitCommit:"59359d9fdce74738ac9a672d2f31e9a346c5cece", GitTreeState:"clean", BuildDate:"2017-10-12T21:53:13Z", GoVersion:"go1.8.3", Compiler:"gc", Platform:"linux/amd64"}
+```
+
+
+28/5/2019, Thijs Kaper.
+
---
+++ tiller-cleanup.sh
@@ -0,0 +1,110 @@
+#!/bin/bash
+
+# Cleanup old helm tiller history config maps.
+# This can be handy if you forgot to set TILLER_HISTORY_MAX on your tiller deployment, and added it later on.
+# 28/5/2019, Thijs Kaper.
+
+# Change the two variables below to match your setup.
+TILLERNAMESPACE=kube-system
+TILLER_HISTORY_MAX=5
+
+echo "Settings: TILLERNAMESPACE=${TILLERNAMESPACE}, TILLER_HISTORY_MAX=${TILLER_HISTORY_MAX}"
+
+if [ "$1" != "-f" -a "$1" != "-n" ]
+then
+   echo "Usage: $0 [-f | -n]"
+   echo "Use -n for a dry-run to show what would be deleted"
+   echo "Use -f to execute a run with actual deletes"
+   exit 1
+fi
+
+function log {
+   echo "`date` $*"
+}
+
+TMPFILE=/tmp/tiller-map-list-$$.lst
+
+# get full list of tiller maps, per line: version tiller-name mapname
+# Example line: 2 watchdog-service watchdog-service.v2
+log "get list of tiller maps"
+kubectl --request-timeout=600s -n ${TILLERNAMESPACE} get cm --no-headers -l OWNER=TILLER \
+        -o jsonpath='{range .items[*]}{.metadata.labels.VERSION}{" "}{.metadata.labels.NAME}{" "}{.metadata.name }{"\n"}{end}' | sort -r -n  >$TMPFILE
+if [ "$?" != "0" ]
+then
+   echo "Error?"
+   exit 1
+fi
+ORIGMAPCOUNT="`cat $TMPFILE | wc -l`"
+log "read $ORIGMAPCOUNT maps"
+
+log "get unique tiller deployment names"
+MAPNAMES="`cat $TMPFILE | awk '{ print $2}' | sort -u`"
+DEPLOYCOUNT="`echo $MAPNAMES | wc -w`"
+log "found $DEPLOYCOUNT tiller deployment names"
+
+# get a list of release label values from active k8s objects
+# these are prefixed and postfixed by a # to enable exact match searching using grep later on
+log "get list of active tiller releases"
+kubectl --request-timeout=600s get all,cronjobs --all-namespaces -l heritage=Tiller -L release -o jsonpath='{range .items[*]}{"#"}{.metadata.labels.release}{"#\n"}{end}' | sort -u >$TMPFILE.active
+if [ "$?" != "0" ]
+then
+   echo "Error?"
+   exit 1
+fi
+ACTIVERELEASECOUNT="`cat $TMPFILE.active | wc -l`"
+log "active tiller release count ${ACTIVERELEASECOUNT}"
+
+deleteCount=0
+for CHECKNAME in $MAPNAMES
+do
+   log "-----------------------------------"
+   log "Check:  $CHECKNAME"
+   grep -q "#${CHECKNAME}#" $TMPFILE.active
+   if [ "$?" == "0" ]
+   then
+      ACTIVE=true
+   else
+      ACTIVE=false 
+      log "Tiller map(s) found, but no deploy? - will remove tiller maps for ${CHECKNAME}"
+   fi
+
+   i=0
+   while read VERSION NAME MAPNAME 
+   do
+      if [ "$CHECKNAME" == "$NAME" ]
+      then
+         i=$((i + 1))
+         if [ $i -gt ${TILLER_HISTORY_MAX} -o "$ACTIVE" == "false" ]
+         then
+            log "Delete: $MAPNAME"
+            deleteCount=$((deleteCount + 1))
+            if [ "$1" == "-f" ]
+            then
+               kubectl delete -n ${TILLERNAMESPACE} configmap $MAPNAME
+            fi
+         else
+            log "Keep:   $MAPNAME"
+         fi
+      fi
+   done <$TMPFILE
+
+done
+
+rm -rf $TMPFILE
+rm -rf $TMPFILE.active
+
+log "---------------------------------------------------"
+log "Original number of maps $ORIGMAPCOUNT"
+log "Original number of tiller deployments $DEPLOYCOUNT"
+log "Active release count ${ACTIVERELEASECOUNT}"
+log "Number of maps deleted $deleteCount"
+log "Number of maps after deletion $((ORIGMAPCOUNT - deleteCount))"
+log "---------------------------------------------------"
+
+if [ "$1" != "-f" ]
+then
+   log "Note: this was a DRY-RUN, so nothing has been deleted..."
+fi
+
+
+
